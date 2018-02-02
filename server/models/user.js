1ef703e6e55b750ab4c026f99cfde6d9e4d40021
Ebuka Umeh <charpellumeh@gmail.com>
2018-02-02 09:24:48 +0100

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -70,9 +70,6 @@ UserSchema.statics.findByToken = function (token) {
   try {
     decoded = jwt.verify(token, 'abc123')
   } catch (e) {
-    // return new Promise((resolve, reject) => {
-    //   reject()
-    // })
     return Promise.reject()
   }
   return User.findOne({
